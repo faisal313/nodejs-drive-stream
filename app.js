24f2a0a1daf43a5f8b80fabc8459bd48920be777
Mohammad Faisal <faisal@Mohammads-MacBook-Air.local>
2023-01-15 11:58:39 +0530

    nudge chunk size

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ function performRequest_default(req, res, access_token, fileInfo) {
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-");
     const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const end =  fileSize - 1;
     const chunksize = end - start + 1;
     console.log("chunksize", { start, end, chunksize, parts });
     const head = {
