00a1cd36cf6b8264e8ddca5c3a8302a0a585c9c4
Mohammad Faisal <faisal@Mohammads-MacBook-Air.local>
2023-01-15 12:43:18 +0530

    nudge chunk size

--- app.js
+++ app.js
@@ -172,15 +172,13 @@ function performRequest_default(req, res, access_token, fileInfo) {
   var fileMime = fileInfo.info.mimeType;
   var fileId = fileInfo.id;
   const range = req.headers.range;
-  console.log("req.headers.range", range);
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-");
     const start = parseInt(parts[0], 10);
-    console.log({ parts, start });
     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
     const chunksize = end - start + 1;
     const head = {
-      "Content-Range": `bytes ${start}-${end/5}/${fileSize}`,
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
       "Accept-Ranges": "bytes",
       "Content-Length": chunksize,
       //'Content-Type': 'video/mp4',
