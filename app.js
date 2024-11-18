cec2959624dad80bbf61891f102b0d7b7f4f33fd
Faisal Khan <faisal_khan@intuit.com>
2024-11-18 20:24:10 +0530

    testing torrent func

--- app.js
+++ app.js
@@ -24,57 +24,66 @@ function calculateByteRangeForDuration(file, durationInSeconds) {
 
 const torrentId = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent';
 
+// Add the torrent only once
+let torrent;
+client.add(torrentId, (_, t) => {
+  torrent = t;
+  console.log('Torrent added:', torrentId);
+});
+
 // WebTorrent streaming route
 app.get('/stream', (req, res) => {
-  client.add(torrentId, (torrent) => {
-    const mp4Files = torrent.files.filter((file) => file.name.endsWith('.mp4'));
+  if (!torrent) {
+    return res.status(500).send('Torrent is not ready yet. Please try again later.');
+  }
 
-    if (mp4Files.length === 0) {
-      return res.status(404).send('No MP4 files found in torrent');
-    }
+  const mp4Files = torrent.files.filter((file) => file.name.endsWith('.mp4'));
 
-    const file = mp4Files[0];
-    console.log(`Streaming file: ${file.name}`);
+  if (mp4Files.length === 0) {
+    return res.status(404).send('No MP4 files found in torrent');
+  }
 
-    const range = req.headers.range;
-    let start, end;
+  const file = mp4Files[0];
+  console.log(`Streaming file: ${file.name}`);
 
-    if (!range) {
-      const initialByteRange = calculateByteRangeForDuration(file, 10); // 10 seconds
-      start = initialByteRange.start;
-      end = initialByteRange.end;
-    } else {
-      const positions = range.replace(/bytes=/, '').split('-');
-      start = parseInt(positions[0], 10);
-      end = positions[1] ? parseInt(positions[1], 10) : file.length - 1;
-    }
+  const range = req.headers.range;
+  let start, end;
 
-    if (start >= file.length || end >= file.length) {
-      res.writeHead(416, {
-        'Content-Range': `bytes */${file.length}`,
-      });
-      return res.end();
-    }
+  if (!range) {
+    const initialByteRange = calculateByteRangeForDuration(file, 10); // 10 seconds
+    start = initialByteRange.start;
+    end = initialByteRange.end;
+  } else {
+    const positions = range.replace(/bytes=/, '').split('-');
+    start = parseInt(positions[0], 10);
+    end = positions[1] ? parseInt(positions[1], 10) : file.length - 1;
+  }
 
-    const chunkSize = end - start + 1;
-
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${file.length}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunkSize,
-      'Content-Type': 'video/mp4',
-    };
-
-    res.writeHead(206, head);
-
-    const stream = file.createReadStream({ start, end });
-
-    stream.on('error', (err) => {
-      res.end(err);
+  if (start >= file.length || end >= file.length) {
+    res.writeHead(416, {
+      'Content-Range': `bytes */${file.length}`,
     });
+    return res.end();
+  }
 
-    stream.pipe(res);
+  const chunkSize = end - start + 1;
+
+  const head = {
+    'Content-Range': `bytes ${start}-${end}/${file.length}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': chunkSize,
+    'Content-Type': 'video/mp4',
+  };
+
+  res.writeHead(206, head);
+
+  const stream = file.createReadStream({ start, end });
+
+  stream.on('error', (err) => {
+    res.end(err);
   });
+
+  stream.pipe(res);
 });
 
 // Express routes
