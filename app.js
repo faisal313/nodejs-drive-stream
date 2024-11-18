927c46e9409523b6288ccdd2a437231dc1f8f016
Faisal Khan <faisal_khan@intuit.com>
2024-11-18 19:50:31 +0530

    testing torrent func

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ app.delete("/keylogs", async (req, res) => {
 
 // Route for streaming torrent
 app.get("/stream-torrent", (req, res) => {
-  const magnetURI = 'magnet:?xt=urn:btih:c9e15763f722f23e98a29decdfae341b98d53056';
+  const magnetURI = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent';
   
   if (!magnetURI) {
     return res.status(400).send("Magnet URI is required");
