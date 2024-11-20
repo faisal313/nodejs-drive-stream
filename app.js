2f6bd9980e2bf41146e6b4e2ae72e92c52cd9d9c
Faisal Khan <faisal_khan@intuit.com>
2024-11-20 22:50:16 +0530

    fix: stream efficiency

--- app.js
+++ app.js
@@ -10,13 +10,8 @@ app.use(cors());
 
 const PORT = 9001;
 
-// Configure WebTorrent client
-const client = new WebTorrent({
-  dht: true,
-  tracker: true,
-  maxConns: 100 // Increase max connections for better peer management
-});
-
+// WebTorrent client
+const client = new WebTorrent();
 const torrentsMap = new Map();
 
 const defaultTorrentId = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent';
@@ -44,12 +39,10 @@ app.get('/stream', async (req, res) => {
       console.log('Adding new torrent...');
       client.add(torrentId, (newTorrent) => {
         torrentsMap.set(newTorrent.infoHash, newTorrent);
-        prioritizePeers(newTorrent);
         handleStreaming(newTorrent, req, res);
       });
     } else {
       console.log('Using existing torrent...');
-      prioritizePeers(torrent);
       handleStreaming(torrent, req, res);
     }
   } catch (err) {
@@ -57,28 +50,6 @@ app.get('/stream', async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-
-// Function to prioritize peers and manage connections
-function prioritizePeers(torrent) {
-  torrent.on('wire', (wire) => {
-    console.log(`Connected to peer ${wire.remoteAddress}`);
-    // Prioritize rare pieces to ensure smooth playback
-    wire.use(createPrioritizeRarePiecesExtension());
-  });
-}
-
-// WebTorrent extension to prioritize rare pieces
-function createPrioritizeRarePiecesExtension() {
-  return {
-    name: 'PrioritizeRarePieces',
-    onWire(wire) {
-      wire.on('download', function (index) {
-        console.log(`Piece ${index} downloaded`);
-        wire.priority(index); // Increase priority for the downloaded piece
-      });
-    }
-  };
-}
 
 function handleStreaming(torrent, req, res) {
   const supportedVideoFileExtensions = ['.mp4', '.m4v', '.mov', '.mkv', '.avi', '.wmv', '.flv', '.webm'];
@@ -96,8 +67,9 @@ function handleStreaming(torrent, req, res) {
   let start, end;
 
   if (!range) {
-    start = 0;
-    end = Math.min(file.length - 1, 10 * 1024 * 1024); // First 10MB as default buffer
+    const initialByteRange = calculateByteRangeForDuration(file, 30); // Larger buffer for 30 seconds
+    start = initialByteRange.start;
+    end = initialByteRange.end;
   } else {
     const positions = range.replace(/bytes=/, '').split('-');
     start = parseInt(positions[0], 10);
@@ -124,15 +96,10 @@ function handleStreaming(torrent, req, res) {
 
   const stream = file.createReadStream({ start, end });
 
-  // Pre-fetch logic for buffering ahead
-  stream.on('end', () => {
-    if (end < file.length - 1) {
-      const prefetchStart = end + 1;
-      const prefetchEnd = Math.min(file.length - 1, end + 10 * 1024 * 1024); // Next 10MB chunk
-      file.createReadStream({ start: prefetchStart, end: prefetchEnd }).on('data', () => {
-        // Pre-fetch buffer
-      }).on('error', (err) => {
-        console.error('Pre-fetch error:', err.message);
+  stream.on('data', (chunk) => {
+    if (!stream.destroyed && end + chunkSize < file.length) {
+      file.createReadStream({ start: end + 1, end: end + 2 * chunkSize }).on('data', (preFetchChunk) => {
+        // Buffer the pre-fetch chunk
       });
     }
   });
